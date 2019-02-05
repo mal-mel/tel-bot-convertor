cc23576004d9cfd3e7d43071abb0f12404c5596a
Oleg Svetovidov <olegsvetovidov@gmail.com>
2019-02-06 00:26:19 +0300

    "init"

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 import telebot
 
 token = '705670307:AAG0SxbaCUFxICH4l9KisZIrTm7ihFFMA00'
-telebot.apihelper.proxy = {'https': '173.249.51.179:3128'}
+# telebot.apihelper.proxy = {'https': '173.249.51.179:3128'}
